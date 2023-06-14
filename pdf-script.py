515a1a81aeb7152afb504b617e87c4f8db7e2ac8
SCXIV <Scarlettcoding@gmail.com>
2023-06-14 15:27:26 -0400

    added code to write table, PDF updated with data

--- pdf-script.py
+++ pdf-script.py
@@ -3,11 +3,7 @@
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
 
-styles = getSampleStyleSheet()
-report = SimpleDocTemplate("pdf/report.pdf")
-report_title = Paragraph("A list of fruits I enjoy", styles["h1"])
-report.build([report_title])
-
+# Data to write into PDF
 fruit = {
     "strawberries": 1,
     "kiwis": 1,
@@ -18,3 +14,22 @@ fruit = {
     "watermelons": 13
 }
 
+# Importing styles from the stylesheet to be used in the PDF
+styles = getSampleStyleSheet()
+
+# This code is used to create the PDF File
+report = SimpleDocTemplate("pdf/report.pdf")
+
+# Setting the title of the PDF report
+report_title = Paragraph("A list of fruits I enjoy", styles["h1"])
+report.build([report_title])
+
+# Initializing Table for PDF
+table_data = []
+
+# Writing data to Table
+for k,v in fruit.items():
+    table_data.append([k, v])
+
+report_table = Table(data=table_data)
+report.build([report_title, report_table])
