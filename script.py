eb6a441768a80170dcd1942b02ad2e06a36db349
SCXIV <Scarlettcoding@gmail.com>
2023-06-14 13:32:20 -0400

    added the code to attach the image

--- script.py
+++ script.py
@@ -13,17 +13,25 @@ sender = "testscript@example.com"
 recipient = os.environ['GMAIL']
 
 # Defining attachable path and name
-attachment_path = "/image/image.jpg"
+attachment_path = "image/image.jpg"
 attachment_filename = os.path.basename(attachment_path)
 
 # Using MIME to define type of file being sent
 mime_type, mime_subtype = mimetypes.guess_type(attachment_path)
 mime_type, mime_subtype = mime_type.split('/', 1)
 
+# The code below creates the email
 message['From'] = sender
 message['To'] = recipient
 message['Subject'] = 'Testing email sending script. Sent from {}'.format(sender)
 body = "Body message of email. Sent from python script on local Linux machine"
 message.set_content(body)
+
+# The code below attaches the picture to the email
+with open(attachment_path, 'rb') as ap:
+    message.add_attachment(ap.read(),
+                           maintype=mime_type,
+                           subtype=mime_subtype,
+                           filename=os.path.basename(attachment_path))
 
 print(message)
